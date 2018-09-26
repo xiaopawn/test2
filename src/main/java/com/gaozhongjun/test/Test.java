7d0c542f38f7d5b74054a301c7236ddde560bc90
gzj <842384656@qq.com>
2018-09-26 22:50:08 +0800

    修改了一个功能

--- src/main/java/com/gaozhongjun/test/Test.java
+++ src/main/java/com/gaozhongjun/test/Test.java
@@ -3,5 +3,7 @@ package com.gaozhongjun.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("11111111");
+
+        System.out.println("22222");
     }
 }
