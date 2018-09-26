732346e6f4d12f9de849cde621258be6ab33b9bd
gzj <842384656@qq.com>
2018-09-26 22:52:48 +0800

    第一次部署

--- src/main/java/com/gaozhongjun/test/Test.java
+++ src/main/java/com/gaozhongjun/test/Test.java
@@ -3,7 +3,7 @@ package com.gaozhongjun.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("11111111");
-
+        System.out.println("33333");
         System.out.println("22222");
     }
 }
